e7bbe05f8430878ef91e899fe38cd7fb1f7c9110
half1980 <panagiotismitropoulos@gmail.com>
2024-11-15 20:08:57 +0200

    a testing function that adds two floating point numbers

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 print("Because i can....! And much more!")
+
+def addition(a: float, b: float) -> float:
+    """This function is used to add to floating point numbers.
+
+    Args:
+        a (float): parameter named a of type float for the function
+        b (float): parameter named a of type float for the function
+
+    Returns:
+        float: Returns the sum of type float, of the parameters a, b
+    """
+    return a + b
+
+print(addition(3,5))
